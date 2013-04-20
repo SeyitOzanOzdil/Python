2e8c0bf4f6724341c77dad358f52de97439738b8
ozan <ozan.ozdil@bil.omu.edu.tr>
2013-04-20 22:50:00 +0300

    new

--- ÇS/LogicalParseTree.py
+++ ÇS/LogicalParseTree.py
@@ -1,3 +1,46 @@
+class Stack :
+    def __init__(self):
+        self.items = []
+    def push (self,item):
+        return self.items.append(item)
+    def isEmpty (self):
+        return self.items == []
+    def pop (self):
+        return self.items.pop()
+    def peek (self):
+        return self.items[len(self.items)-1]
+
+def BinaryTree(r):
+    return [r,[],[]]
+
+def insertLeft(root,newBranch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1,newranch,t,[]])
+    else:
+        root.insert(1,[newBranch,[],[]])
+    return root
+
+def insertRight(root,newBranch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2,[newranch],t,[]])
+    else:
+        root.insert(1,[newBranch],[],[]])
+    return root
+
+def getRootVal(root):
+    return root[0]
+
+def setRootVal(root,newVal):
+    root[0] = newVal
+
+def getLeftChild(root):
+    return root[1]
+
+def getRightChild(root):
+    return root[2]
+
 def buildParseTree(fpexp):
     fplist = fpexp.split()
     pStack = Stack()
